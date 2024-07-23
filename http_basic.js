4273ee0d9510b2ed0265be7d57b357d089716b62
Daniel Walter <75435511+Dannspress@users.noreply.github.com>
2024-07-23 18:21:39 +0000

    add/adiciona check

--- http_basic.js
+++ http_basic.js
@@ -1,5 +1,14 @@
 import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '3s',
+}
 
 export default function () {
-    http.get('http://test.k6.io');
+    const res = http.get('http://test.k6.io');
+    check(res, {
+        'status code is 200': (r) => r.status === 200,
+    })
 }
